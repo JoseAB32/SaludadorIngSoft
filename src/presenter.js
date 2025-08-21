6a5d38b32be45887ef26bfdbbf60cfc92ab29e57
JoseAB32 <arias.balderrama.josmearia@gmail.com>
2025-08-21 09:02:36 -0400

    feat: Saluda Con parametros de edad

--- src/presenter.js
+++ src/presenter.js
@@ -3,6 +3,7 @@ import saludar from "./saludador.js";
 const nombre = document.querySelector("#nombre");
 const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#resultado-div");
+const edad = document.querySelector("#edad");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -11,7 +12,9 @@ form.addEventListener("submit", (event) => {
 
   const Genero = document.querySelector('input[name="Genero"]:checked')?.value || "Otro";
 
+  const Edad = edad.value;
+
   console.log(Nombre, Genero);
 
-  div.innerHTML = "<p> HOLA " + saludar(Nombre, Genero) + "</p>";
+  div.innerHTML = "<p> HOLA " + saludar(Nombre, Genero, Edad) + "</p>";
 });
--- src/saludador.js
+++ src/saludador.js
@@ -1,19 +1,28 @@
 const fechaActual = new Date();
 const hora = fechaActual.getHours();
 
-function mesnajeGenero(genero) {
+function mesnajeGenero(genero, edad) {
   if (genero === "Masculino") {
+    if (edad > 30) {
+      return "un placer tenerte aquí Sr.";
+    }
     return "un placer tenerlo aquí";
   } else if (genero === "Femenino") {
+    if (edad > 30) {
+      return "un placer tenerte aquí Sra.";
+    }
     return "un placer tenerla aquí";    
   }
+    if (edad > 30) {    
+      return "un placer tenerle aquí Estimado";
+    }
     return "un placer tenerle aquí";
 }
 
 
-function saludar(nombre, genero) {
+function saludar(nombre, genero, edad) {
 
-    const mensaje = mesnajeGenero(genero);
+    const mensaje = mesnajeGenero(genero, edad);
     if (hora < 12) {
         return `Buenos días ${nombre}, ${mensaje}`;
     } else if (hora < 18) {
